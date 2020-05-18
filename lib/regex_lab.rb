8127e6e14b001cdef70a572f0ec368a6d03998e5
Matt Farncombe <matt@farncombe.co>
2020-05-18 13:35:56 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone.delete("_")
+  phone.delete("_", "(", "(")
   puts phone
   if phone.match(/\d{7}/)
     return true
